7ce3b6f12d81d6c0436b4bf76878456545e34f7f
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-08-31 08:33:33 +0200

    added names to scoreboard added updating of names based on score

--- SmurfTracker.cpp
+++ SmurfTracker.cpp
@@ -1,21 +1,9 @@
 #include "pch.h"
 #include "SmurfTracker.h"
-#include <fstream>
-#include <ctime>
-#include <iomanip>
 
 BAKKESMOD_PLUGIN(SmurfTracker, "Identify Smurfs.", plugin_version, PLUGINTYPE_FREEPLAY)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
-bool smurfTrackerEnabled = false;
-
-struct PlayerDetails {
-	std::string playerName;
-	std::string platform;
-	std::string uniqueID;
-	int playerIndex = 0;
-	int wins = 0;	
-};
 
 std::string getCurrentTime() {
 	std::time_t t = std::time(nullptr);
@@ -25,16 +13,31 @@ std::string getCurrentTime() {
 	return ss.str();
 }
 
+void SmurfTracker::LogF(const std::string& message) {
+	if (logFile.is_open()) {
+		logFile << "[" << getCurrentTime() << "] " << message << std::endl;
+	}
+	else {
+		LOG("Log file is not open!");
+	}
+}
+
 void SmurfTracker::onLoad()
 {
+	_globalCvarManager = cvarManager;
+	LOG("SmurfTracker loaded!");
+	DEBUGLOG("SmurfTracker debug mode enabled"); // logging.h DEBUG_LOG = true;
+
+	// Open the log file in append mode (Location is Epic Games\rocketleague\Binaries\Win64\SmurfTracker.log)
+	logFile.open("SmurfTracker.log", std::ios::app);
+	if (!logFile.is_open()) {
+		LOG("Failed to open log file!");
+	}
+
 	// Register the render function to be called each frame
 	gameWrapper->RegisterDrawable([this](CanvasWrapper canvas) {
 		Render(canvas);
 		});
-
-	_globalCvarManager = cvarManager;
-	LOG("SmurfTracker loaded!");
-	DEBUGLOG("SmurfTracker debug mode enabled"); // logging.h DEBUG_LOG = true;
 	
 	cvarManager->registerCvar("SmurfTracker_enabled", "0", "Enable SmurfTracker Plugin", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
@@ -43,6 +46,10 @@ void SmurfTracker::onLoad()
 
 	cvarManager->registerNotifier("DisplayPlayerIDs", [this](std::vector<std::string> args) {
 		DisplayPlayerIDs();
+		}, "", PERMISSION_ALL);
+
+	cvarManager->registerNotifier("InitializeCurrentPlayers", [this](std::vector<std::string> args) {
+		InitializeCurrentPlayers();
 		}, "", PERMISSION_ALL);
 
 	// Updated the HTTPRequest notifier to always expect a URL argument
@@ -58,30 +65,199 @@ void SmurfTracker::onLoad()
 		}
 		}, "", PERMISSION_ALL);
 
-	// Hook into the StartRound event to display player IDs at the start of every kickoff
-	gameWrapper->HookEvent("Function GameEvent_Soccar_TA.Active.StartRound", [this](std::string eventName) {
-		cvarManager->executeCommand("DisplayPlayerIDs");
+	// Hook into the OnAllTeamsCreated event to log when all teams are created	
+	gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.OnAllTeamsCreated", [this](std::string eventName) {
+		LOG("Initialize Game Session");
+		cvarManager->executeCommand("InitializeCurrentPlayers");
 		});
 
+	// Hook into the StartRound event to display player IDs at the start of every kickoff
+	//gameWrapper->HookEvent("Function GameEvent_Soccar_TA.Active.StartRound", [this](std::string eventName) {
+	//	cvarManager->executeCommand("DisplayPlayerIDs");
+	//	});
+
 	// Hook into the BeginState event to display player IDs at the start of a kickoff countdown or beginning/reset of freeplay
-	gameWrapper->HookEvent("Function GameEvent_Soccar_TA.Countdown.BeginState", [this](std::string eventName) {
-		cvarManager->executeCommand("DisplayPlayerIDs");
-		});
+	//gameWrapper->HookEvent("Function GameEvent_Soccar_TA.Countdown.BeginState", [this](std::string eventName) {
+	//	cvarManager->executeCommand("DisplayPlayerIDs");
+	//	});
 
 	// Hook into the OnOpenScoreboard event to display player IDs when the scoreboard is opened
 	gameWrapper->HookEvent("Function TAGame.GFxData_GameEvent_TA.OnOpenScoreboard", [this](std::string eventName) {
-		cvarManager->executeCommand("DisplayPlayerIDs");
+		//cvarManager->executeCommand("DisplayPlayerIDs");
+		isSBOpen = true;
+		UpdatePlayerList();
 		});
 
-	// Hook into the OnCloseScoreboard event to log a message when the scoreboard is closed
+	// Hook into the OnCloseScoreboard event to log a message when the scoreboard is closeds
 	gameWrapper->HookEvent("Function TAGame.GFxData_GameEvent_TA.OnCloseScoreboard", [this](std::string eventName) {
-		LOG("Scoreboard closed!");
+		isSBOpen = false;
+		});
+
+	// Hook into the OnMatchEnded event to remove cached player details
+	gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.OnMatchEnded", [this](std::string eventName) {
+		LOG("Finalize Game Session");
+		ClearCurrentPlayers();
 		});
 }
 
-void SmurfTracker::onUnload()
+void SmurfTracker::InitializeCurrentPlayers()
 {
-	LOG("SmurfTracker unloaded!");
+	if (!smurfTrackerEnabled) {
+		return;
+	}
+
+	// Clear any existing players
+	currentPlayers.clear();
+
+	// Check if the game is valid
+	if (!gameWrapper->IsInOnlineGame() && !gameWrapper->IsInFreeplay() || gameWrapper->IsInReplay()) {
+		LOG("Not in an online game or freeplay!");
+		return;
+	}
+
+	ServerWrapper sw = NULL;
+	if (gameWrapper->IsInFreeplay()) {
+		sw = gameWrapper->GetGameEventAsServer();
+	}
+	else {
+		sw = gameWrapper->GetOnlineGame();
+	}
+
+	if (sw.IsNull() || sw.GetbMatchEnded()) {
+		LOG("Invalid game state or match ended!");
+		return;
+	}
+
+	// Open the log file in append mode (Location is Epic Games\rocketleague\Binaries\Win64\SmurfTracker.log)
+	std::ofstream logFile("SmurfTracker.log", std::ios::app);
+
+	// Initialize a map to store unique IDs and corresponding player details
+	std::map<std::string, PlayerDetails> uniqueIDMap;
+
+	// Get the array of players
+	ArrayWrapper<PriWrapper> players = sw.GetPRIs();
+
+	// Iterate through the players
+	for (size_t i = 0; i < players.Count(); i++)
+	{
+		PriWrapper playerWrapper = players.Get(i);
+		if (playerWrapper.IsNull()) continue;
+
+		UniqueIDWrapper uniqueID = playerWrapper.GetUniqueIdWrapper();
+		std::string uniqueIDString = uniqueID.GetIdString();
+
+		// Extract player information (you can add more fields as needed)
+		PlayerDetails details;
+		details.playerName = playerWrapper.GetPlayerName().ToString();
+		details.uniqueID = uniqueIDString;
+		details.playerIndex = static_cast<int>(i);  // Assuming i is the player index
+		details.wins = 0;  // TODO: Get wins from tracker network or other source
+		details.team = playerWrapper.GetTeamNum();  // 0 is blue, 1 is orange
+
+		// Find separators
+		size_t firstSeparator = uniqueIDString.find('|');
+		size_t secondSeparator = uniqueIDString.find('|', firstSeparator + 1);
+
+		// Check for valid separators
+		if (firstSeparator == std::string::npos || secondSeparator == std::string::npos || firstSeparator >= secondSeparator) {
+			LOG("Invalid unique ID format: " + uniqueIDString);
+			continue; // Skip this player if the unique ID format is not as expected
+		}
+
+		std::string uniqueIDPart = uniqueIDString.substr(firstSeparator + 1, secondSeparator - firstSeparator - 1);
+		int playerIndex = static_cast<int>(std::stoi(uniqueIDString.substr(secondSeparator + 1)));
+
+		LogF("Player name: " + details.playerName +
+			" | Platform: " + details.platform +
+			" | ID: " + details.uniqueID +
+			" | PlayerIndex: " + std::to_string(details.playerIndex));
+
+		// If this is a main player, store the details
+		if (playerIndex == 0) {
+			uniqueIDMap[uniqueIDPart] = details;
+		}
+		// If this is a splitscreen player, retrieve the main player's details
+		else {
+			if (uniqueIDMap.find(uniqueIDPart) != uniqueIDMap.end()) {
+				details = uniqueIDMap[uniqueIDPart];
+				// Log the splitscreen relationship
+				LOG("Splitscreen player of main player at index " + std::to_string(details.playerIndex));
+				continue;
+			}
+			else {
+				LOG("Main player's details not found for splitscreen player: " + uniqueIDString);
+				continue;
+			}
+		}
+
+		// Add the player details to the currentPlayers vector
+		currentPlayers.push_back(details);
+	}
+
+	UpdateTeamStrings();
+}
+
+void SmurfTracker::ClearCurrentPlayers()
+{
+	currentPlayers.clear();
+}
+
+void SmurfTracker::UpdatePlayerList() {
+	//TODO: Check if all players are still connected and/or if new players have joined
+
+	ServerWrapper sw = NULL;
+	if (gameWrapper->IsInFreeplay()) {
+		sw = gameWrapper->GetGameEventAsServer();
+	}
+	else {
+		sw = gameWrapper->GetOnlineGame();
+	}
+
+	if (sw.IsNull() || sw.GetbMatchEnded()) {
+		LOG("Invalid game state or match ended!");
+		return;
+	}
+
+	// Get the array of players
+	ArrayWrapper<PriWrapper> players = sw.GetPRIs();
+
+	for (PlayerDetails& player : currentPlayers) {
+		for (size_t i = 0; i < players.Count(); ++i) {
+			PriWrapper priw = players.Get(i);
+			if (priw.IsNull()) continue;
+
+			UniqueIDWrapper uidw = priw.GetUniqueIdWrapper();
+			std::string uidString = uidw.GetIdString();
+			if (uidString == player.uniqueID) {
+				player.currentScore = priw.GetMatchScore();
+				break;  // Exit the inner loop as we've found the matching player
+			}
+		}
+	}
+
+	// Sort the players
+	std::sort(currentPlayers.begin(), currentPlayers.end(), [](const PlayerDetails& a, const PlayerDetails& b) {
+		if (a.team == b.team) {
+			return a.currentScore > b.currentScore;  // Sort by currentScore within the same team
+		}
+		return a.team < b.team;  // Sort by team (Blue before Orange)
+		});
+
+	UpdateTeamStrings();
+}
+
+void SmurfTracker::UpdateTeamStrings() {
+	blueTeam.clear();
+	orangeTeam.clear();
+
+	for (const auto& player : currentPlayers) {
+		if (player.team == 0) {
+			blueTeam.push_back(player.playerName);
+		}
+		else if (player.team == 1) {
+			orangeTeam.push_back(player.playerName);
+		}
+	}
 }
 
 void SmurfTracker::HTTPRequest(const std::string& url)
@@ -109,16 +285,37 @@ void SmurfTracker::HTTPRequest(const std::string& url)
 	*logFile << "[" << getCurrentTime() << "] End of request" << std::endl;
 }
 
-// Function to render text next to the scoreboard
 void SmurfTracker::Render(CanvasWrapper canvas)
 {
-	// defines colors in RGBA 0-255
-	LinearColor colors;
-	colors.R = 255;
-	colors.G = 255;
-	colors.B = 255;
-	colors.A = 255;
-	canvas.SetColor(colors);
+	if (!smurfTrackerEnabled || !isSBOpen) {
+		return;
+	}
+
+	if (!gameWrapper->IsInOnlineGame() && !gameWrapper->IsInFreeplay() || gameWrapper->IsInReplay()) {
+		return;
+	}
+
+	if (currentPlayers.size() < 1) {
+		InitializeCurrentPlayers();
+		return;
+	}
+
+	ServerWrapper sw = NULL;
+	if (gameWrapper->IsInFreeplay()) {
+		sw = gameWrapper->GetGameEventAsServer();
+	}
+	else {
+		sw = gameWrapper->GetOnlineGame();
+	}
+	if (sw.IsNull() || sw.GetbMatchEnded()) {
+		return;
+	}
+
+	if (currentPlayers.size() < sw.GetPRIs().Count()) {
+		LogF("Not all players found! " + std::to_string(currentPlayers.size()) + "in Array vs.: " + std::to_string(sw.GetPRIs().Count()));
+		InitializeCurrentPlayers();
+		return;
+	}
 
 	// Get the screen size
 	int screenWidth = canvas.GetSize().X;
@@ -127,11 +324,61 @@ void SmurfTracker::Render(CanvasWrapper canvas)
 	// Calculate the position next to the scoreboard
 	Vector2 scoreboardPosition(screenWidth - 500, screenHeight / 2);
 
-	std::string TextToDisplay = "SmurfTracker";
-	
-	// Draw the text
+	std::string TextToDisplay = "Connected Players:" + std::to_string(currentPlayers.size()) + "/" + std::to_string(sw.GetPRIs().Count());
+	LinearColor white;
+	white.R = 255;
+	white.G = 255;
+	white.B = 255;
+	white.A = 255;
+	canvas.SetColor(white);
 	canvas.SetPosition(scoreboardPosition);
 	canvas.DrawString(TextToDisplay);
+
+	// Initialize vertical offset for player names
+	int verticalOffset = 20;
+
+	// Update the vertical position for the next line
+	scoreboardPosition.Y += verticalOffset;
+
+	// Draw Blue Team
+	LinearColor blue;
+	blue.R = 0;
+	blue.G = 0;
+	blue.B = 255;
+	blue.A = 255;
+	canvas.SetColor(blue);
+	canvas.SetPosition(scoreboardPosition);
+	canvas.DrawString("Blue:");
+
+	// Update the vertical position for the next line
+	scoreboardPosition.Y += verticalOffset;
+	canvas.SetColor(white);
+
+	for (const auto& player : blueTeam) {
+		canvas.SetPosition(scoreboardPosition);
+		canvas.DrawString(player);
+		scoreboardPosition.Y += verticalOffset;
+	}
+
+	// Draw Orange Team
+	LinearColor orange;
+	orange.R = 255;
+	orange.G = 165;
+	orange.B = 0;
+	orange.A = 255;
+	canvas.SetColor(orange);
+	canvas.SetPosition(scoreboardPosition);
+	canvas.DrawString("Orange:");
+
+	// Update the vertical position for the next line
+	scoreboardPosition.Y += verticalOffset;
+	canvas.SetColor(white);
+
+	for (const auto& player : orangeTeam) {
+		canvas.SetPosition(scoreboardPosition);
+		canvas.DrawString(player);
+		scoreboardPosition.Y += verticalOffset;
+	}
 }
 
 void SmurfTracker::DisplayPlayerIDs()
@@ -159,58 +406,11 @@ void SmurfTracker::DisplayPlayerIDs()
 		return;
 	}
 
-	// Get the array of players
-	ArrayWrapper<PriWrapper> players = sw.GetPRIs();
-
 	// Open the log file in append mode (Location is Epic Games\rocketleague\Binaries\Win64\SmurfTracker.log)
 	std::ofstream logFile("SmurfTracker.log", std::ios::app);
 
-	std::map<std::string, PlayerDetails> uniqueIDMap;
-
-	// Iterate through the players
-	for (size_t i = 0; i < players.Count(); i++)
-	{
-		PriWrapper playerWrapper = players.Get(i);
-		if (playerWrapper.IsNull()) continue;
-
-		UniqueIDWrapper uniqueID = playerWrapper.GetUniqueIdWrapper();
-		std::string uniqueIDString = uniqueID.GetIdString();
-
-		// Find separators
-		size_t firstSeparator = uniqueIDString.find('|');
-		size_t secondSeparator = uniqueIDString.find('|', firstSeparator + 1);
-
-		// Check for valid separators
-		if (firstSeparator == std::string::npos || secondSeparator == std::string::npos || firstSeparator >= secondSeparator) {
-			LOG("Invalid unique ID format: " + uniqueIDString);
-			continue; // Skip this player if the unique ID format is not as expected
-		}
-
-		std::string platform = uniqueIDString.substr(0, firstSeparator);
-		std::string uniqueIDPart = uniqueIDString.substr(firstSeparator + 1, secondSeparator - firstSeparator - 1);
-		int playerIndex = static_cast<int>(std::stoi(uniqueIDString.substr(secondSeparator + 1)));
-
-		PlayerDetails details;
-
-		// If this is a main player, store the details
-		if (playerIndex == 0) {
-			details = { playerWrapper.GetPlayerName().ToString(), platform, uniqueIDPart, playerIndex };
-			uniqueIDMap[uniqueIDPart] = details;
-		}
-		// If this is a splitscreen player, retrieve the main player's details
-		else {
-			// Check if the main player's details are in the map
-			if (uniqueIDMap.find(uniqueIDPart) != uniqueIDMap.end()) {
-				details = uniqueIDMap[uniqueIDPart];
-				// Log the splitscreen relationship
-				LOG("Splitscreen player of main player at index " + std::to_string(details.playerIndex));
-				continue;
-			}
-			else {
-				LOG("Main player's details not found for splitscreen player: " + uniqueIDString);
-				continue;
-			}
-		}
+	// Iterate through the currentPlayers vector to log or display the details
+	for (const auto& details : currentPlayers) {
 
 		// Log the details
 		std::string logMessage = "Player name: " + details.playerName +
@@ -221,13 +421,13 @@ void SmurfTracker::DisplayPlayerIDs()
 		logFile << "\n[" << getCurrentTime() << "] " << logMessage << std::endl;
 
 		// Check the platform in the unique ID and log the appropriate request URL
-		if (uniqueIDString.find("Epic") != std::string::npos) {
+		if (details.uniqueID.find("Epic") != std::string::npos) {
 			LOG("Request: https://rocketleague.tracker.network/rocket-league/profile/epic/" + details.playerName);
 		}
-		else if (uniqueIDString.find("PS4") != std::string::npos) {
+		else if (details.uniqueID.find("PS4") != std::string::npos) {
 			LOG("Request: https://rocketleague.tracker.network/rocket-league/profile/psn/" + details.playerName);
 		}
-		else if (uniqueIDString.find("Switch") != std::string::npos) {
+		else if (details.uniqueID.find("Switch") != std::string::npos) {
 			LOG("Request: https://rocketleague.tracker.network/rocket-league/profile/switch/" + details.playerName);
 		}
 
@@ -258,4 +458,12 @@ void SmurfTracker::DisplayPlayerIDs()
 
 	// Close the log file
 	logFile.close();
+}
+
+void SmurfTracker::onUnload()
+{
+	if (logFile.is_open()) {
+		logFile.close();
+	}
+	LOG("SmurfTracker unloaded!");
 }
--- SmurfTracker.h
+++ SmurfTracker.h
@@ -4,27 +4,49 @@
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
+#include <string>
+#include <vector>
+#include <fstream>
 
 #include "version.h"
+
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
+struct PlayerDetails {
+	std::string playerName;
+	std::string platform;
+	std::string uniqueID;
+	int playerIndex = 0;
+	int wins = 0;
+	int currentScore = 0;
+	int team;
+};
 
-class SmurfTracker: public BakkesMod::Plugin::BakkesModPlugin
-	,public SettingsWindowBase
-	//,public PluginWindowBase // Uncomment if you want to render your own plugin window
+std::string getCurrentTime();
+
+class SmurfTracker : public BakkesMod::Plugin::BakkesModPlugin
+	, public SettingsWindowBase
 {
-
-	//std::shared_ptr<bool> enabled;
-
-	//Boilerplate
+	// Boilerplate
 	void onLoad() override;
 	void onUnload() override;
+
 	void DisplayPlayerIDs();
 	void HTTPRequest(const std::string& url);
 	void Render(CanvasWrapper canvas);
+	void InitializeCurrentPlayers();
+	void ClearCurrentPlayers();
+	void LogF(const std::string& message);
+	void UpdatePlayerList();
+	void UpdateTeamStrings();	
 
+	bool isSBOpen;
+	bool smurfTrackerEnabled;
+	std::vector<PlayerDetails> currentPlayers;
+	std::ofstream logFile;
+	std::vector<std::string> blueTeam;
+	std::vector<std::string> orangeTeam;
 
 public:
 	void RenderSettings() override;
-	//void RenderWindow() override; // Uncomment if you want to render your own plugin window
 };
